7b08effdfea62c753b72f7bc3ece1d93b1d7c2a8
froy <froy>
2019-06-16 00:17:40 +0400

    final final version

--- src/main/java/recipemanager/presentation_layer/Main.java
+++ src/main/java/recipemanager/presentation_layer/Main.java
@@ -4,26 +4,6 @@ public class Main
 {
     public static void main( String[] args )
     {
-        //VegetableDAO vegetableDAO = new VegetableDAO();
-        //vegetableDAO.create("Огурец", 15, 1); - работает
-        //vegetableDAO.getById(2); - работает
-        //vegetableDAO.getByTitle("Морковь"); - работает
-        //vegetableDAO.getAllVegetables(); - работает
-        //vegetableDAO.remove(2); - работает
-
-        //SaladRecipeDAO recipeDAO = new SaladRecipeDAO();
-        //recipeDAO.getAllRecipes(); - работает
-        //recipeDAO.create("Винегрет"); //- работает
-        //recipeDAO.remove(8); //- работает
-        //recipeDAO.getById(8); - работает
-
-        //IngredientDAO ingredientDAO = new IngredientDAO();
-        //ingredientDAO.create(9, 7, 300.0); - работает
-        //ingredientDAO.getAllIngredients(); - работает
-        //ingredientDAO.getById(19); - работает
-        //ingredientDAO.removeIngredientsWithRecipeId(9); - работает
-        //ingredientDAO.remove(21); - работает
-
         Chef chef = new Chef();
         chef.showOptions();
     }
